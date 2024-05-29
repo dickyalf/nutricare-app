3883aeeae318f61d67eefd3ee1f6e0b6ac87f3ec
hayyaoe <hayya947@gmail.com>
2024-05-29 19:35:10 +0700

    added livewire, and detail donation view created

--- routes/web.php
+++ routes/web.php
@@ -2,10 +2,14 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('index');
+Route::get("/", function () {
+    return view("index");
 });
 
-Route::get('/donation', function () {
-    return view('donation-listing');
+Route::get("/donation", function () {
+    return view("donation-listing");
 })->name("donation");
+
+Route::get("/donation-detail", function () {
+    return view("donation-detail");
+})->name("donation-detail");
