5daed7dc35eb4aa0d067c526f1ff322cd844857b
hayyaoe <hayya947@gmail.com>
2024-05-31 20:14:07 +0700

    information: menambahkan beberapa deskripsi fungsi

--- app/Livewire/InputPorsi.php
+++ app/Livewire/InputPorsi.php
@@ -8,13 +8,15 @@ use App\Models\Makanan;
 
 class InputPorsi extends Component
 {
+    // property dari class input porsi
     public $makanan;
     public $porsi = 0;
     public $totalDonasi = 0;
 
+    // mengabil nilai dari view, dimana komponen ditempatkan
     public function mount(Makanan $makanan)
     {
-        $this->makanan = $makanan;
+        $this->makanan = $makanan; // memasukkan nilai kedalam variabel makanan.
     }
 
     public function render()
@@ -25,6 +27,6 @@ class InputPorsi extends Component
     // fungsi ini diperuntukkan untuk mengupdate nilai dari totalDonasi, fungsi ini diaktifkan ketika input dilakukan oleh user pada komponen livewire
     public function pilih()
     {
-        $this->totalDonasi = (int) $this->porsi * $this->makanan->harga;
+        $this->totalDonasi = (int) $this->porsi * $this->makanan->harga; // mehintung nilai dari variabel totalDonasi, totalDonasi sama dengan jumlah porsi dikali harga makanan.
     }
 }
